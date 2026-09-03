8baf8e1053a3ccf0a54fbd536d8231c941718000
G Bhavana <bhavanakv2crpf@gmail.com>
2026-09-03 20:56:12 +0530

    Time: 3 ms (73.18%), Space: 47.4 MB (36.44%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ public class BSTIterator{
+    private Stack<TreeNode>st=new Stack<TreeNode>();
+    boolean reverse=true;
+    public BSTIterator(TreeNode root,boolean reverse){
+        this.reverse=reverse;
+        pushAll(root);
+    }
+    public boolean hasNext(){
+        return !st.isEmpty();
+    }
+    public int next(){
+        TreeNode tmp=st.pop();
+        if(reverse==false) pushAll(tmp.right);
+        else pushAll(tmp.left);
+        return tmp.val;
+    }
+    public void pushAll(TreeNode root){
+        while(root!=null){
+            st.push(root);
+            if(reverse==true){
+                root=root.right;
+            }
+            else{
+                root=root.left;
+            }
+        }
+    }
+ }
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if(root==null) return false;
+        BSTIterator l=new BSTIterator(root,false);
+        BSTIterator r=new BSTIterator(root,true);
+        int i=l.next();
+        int j=r.next();
+        while(i<j){
+            if(i+j==k){
+                return true;
+            }
+            else if(i+j>k){
+                j=r.next();
+            }
+            else{
+                i=l.next();
+            }
+        }
+        return false;
+    }
+}
